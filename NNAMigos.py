1d2d70cd7806a927a1d5f22c4c4c69689d6d3507
Adrian Rodriguez Aguiñaga <147747218+AdrianAguinaga@users.noreply.github.com>
2023-11-17 16:43:05 -0800

    Add files via upload

---
+++ NNAMigos.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Nadam
+import tensorflow as tf
+
+tf.test.is_gpu_available(
+    cuda_only=False, min_cuda_compute_capability=None
+)
+# Load the data
+df = pd.read_csv('c:/datos/Usuario5EEG.csv')
+
+# Replace NaN values with the mean of the column
+df.fillna(df.mean(), inplace=True)
+
+# Separate features and labels
+X = df.drop(columns=['Class'])
+y = df['Class']
+
+# Split into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Normalize the data
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Create the model with the best parameters
+model = Sequential([
+    Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
+    Dense(32, activation='relu'),
+   # Dense(32, activation='relu'),
+    Dense(4, activation='softmax')
+])
+
+# Compile the model with Nadam optimizer
+optimizer = Nadam()
+model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model with the best parameters
+history = model.fit(X_train, y_train, epochs=50000, batch_size=30, validation_data=(X_test, y_test), verbose=1)
+
+# Plot the training and validation loss over epochs
+plt.figure(figsize=(10, 6))
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.title('Loss Over Epochs')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig('loss_plot.png')
+plt.show()
+
+# Plot the training and validation accuracy over epochs
+plt.figure(figsize=(10, 6))
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.title('Accuracy Over Epochs')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.savefig('accuracy_plot.png')
+plt.show()
+
+# Get the predictions of the model
+y_pred = np.argmax(model.predict(X_test), axis=-1)
+
+# Calculate statistics
+accuracy = accuracy_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred, average='macro')
+precision = precision_score(y_test, y_pred, average='macro')
+f1 = f1_score(y_test, y_pred, average='macro')
+conf_matrix = confusion_matrix(y_test, y_pred)
+
+print(f"Accuracy: {accuracy:.4f}")
+print(f"Recall: {recall:.4f}")
+print(f"Precision: {precision:.4f}")
+print(f"F1-Score: {f1:.4f}")
+
+# Plot and save confusion matrix
+plt.figure(figsize=(10, 6))
+sns.heatmap(conf_matrix, annot=True, cmap='Blues', fmt='g')
+plt.xlabel('Predicted labels')
+plt.ylabel('True labels')
+plt.title('Confusion Matrix')
+plt.savefig('confusion_matrix.png')
+plt.show()
+
+# Save statistics to a CSV
+stats_df = pd.DataFrame({
+    "Metric": ["Accuracy", "Recall", "Precision", "F1-Score"],
+    "Value": [accuracy, recall, precision, f1]
+})
+stats_df.to_csv('training_statistics.csv', index=False)
+
+
